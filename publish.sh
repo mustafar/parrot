69d7c37cf10b16f89e7477c6d576897cea669034
Mustafa Rizvi <mstfrzv@gmail.com>
2018-03-04 15:53:32 -0500

    chore: add publish script

---
+++ publish.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -o errexit
+
+docker_image_name='mustafar/parrot'
+
+if [ -z $1 ]; then
+  echo "release type not provided"
+  exit 1
+fi
+
+if [ $1 == "patch" ]; then
+  echo "starting patch release"
+  npm version patch
+elif [ $1 == "minor" ]; then
+  echo "starting minor release"
+  npm version minor
+elif [ $1 == "major" ]; then
+  echo "starting major release"
+  npm version major
+else
+  echo "invalid release type"
+  exit 1
+fi
+
+npm run build && npm run test:setup
+version=v$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo "version: $version"
+
+docker tag $docker_image_name $docker_image_name:$version
+docker push $docker_image_name
+
+git commit -am "chore: $1 release" package.json --no-verify
+git tag $version
+git push origin master
+git push origin $version
