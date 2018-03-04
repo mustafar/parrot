a0331fffce1a41ab64a765d1580441168d26e1bb
Mustafa Rizvi <mstfrzv@gmail.com>
2018-03-04 10:07:11 -0500

    feat: mockability

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -2,6 +2,10 @@ const fetch = require('node-fetch');
 
 const apiBase = 'http://localhost:15009/parrot-test';
 
+beforeEach(async () => {
+  await fetch(`${apiBase}/mock`, { method: 'DELETE' });
+});
+
 test('if invalid path, then 501', async () => {
   const response = await fetch(`${apiBase}/foo/bar`);
   expect(response.status).toEqual(501);
@@ -23,8 +27,67 @@ test('if valid path, but not faked and without examples, then 501', async () => 
  * 201: example at the wrong level
  * 200: example at the right level
  */
-test('if not faked, return 1st example response', async () => {
+test('if not mocked, return 1st example response', async () => {
   const response = await fetch(`${apiBase}/batman/location`);
   expect(response.status).toEqual(200);
   expect(response.text()).resolves.toEqual('batcave');
+});
+
+test('if mocked, with body, returns the mocked status and body', async () => {
+  let response = await fetch(`${apiBase}/batman/location`);
+  expect(response.text()).resolves.toEqual('batcave');
+
+  const mock = {
+    method: 'GET', path: '/batman/location', status: 201, response: { arkham: 'asylum' },
+  };
+  response = await fetch(
+    `${apiBase}/mock`,
+    { method: 'PUT', body: JSON.stringify(mock), headers: { 'content-type': 'application/json' } },
+  );
+  expect(response.status).toEqual(204);
+
+  response = await fetch(`${apiBase}/batman/location`, { method: 'GET' });
+  expect(response.status).toEqual(mock.status);
+  expect(response.json()).resolves.toEqual(mock.response);
+});
+
+test('if mocked, without body, returns the mocked status', async () => {
+  let response = await fetch(`${apiBase}/batman/location`);
+  expect(response.text()).resolves.toEqual('batcave');
+
+  const mock = {
+    method: 'GET', path: '/batman/location', status: 201,
+  };
+  response = await fetch(
+    `${apiBase}/mock`,
+    { method: 'PUT', body: JSON.stringify(mock), headers: { 'content-type': 'application/json' } },
+  );
+  expect(response.status).toEqual(204);
+
+  response = await fetch(`${apiBase}/batman/location`, { method: 'GET' });
+  expect(response.status).toEqual(mock.status);
+  expect(response.text()).resolves.toEqual('');
+});
+
+test('if mocked, subsequent mocks will override', async () => {
+  const mock = {
+    method: 'GET', path: '/batman/location', status: 201, response: 'arkham',
+  };
+  let response = await fetch(
+    `${apiBase}/mock`,
+    { method: 'PUT', body: JSON.stringify(mock), headers: { 'content-type': 'application/json' } },
+  );
+  expect(response.status).toEqual(204);
+
+  mock.status = 200;
+  mock.response = 'wayne manor';
+  response = await fetch(
+    `${apiBase}/mock`,
+    { method: 'PUT', body: JSON.stringify(mock), headers: { 'content-type': 'application/json' } },
+  );
+  expect(response.status).toEqual(204);
+
+  response = await fetch(`${apiBase}/batman/location`, { method: 'GET' });
+  expect(response.status).toEqual(200);
+  expect(response.text()).resolves.toEqual('wayne manor');
 });
--- index.js
+++ index.js
@@ -2,12 +2,77 @@ import { findKey, get as getOrDefault } from 'lodash';
 
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const swaggerMiddleware = require('swagger-express-middleware');
 const interceptor = require('express-interceptor');
 
 const app = express();
 const port = process.env.PORT;
 const swaggerPath = process.env.SWAGGER_SPEC;
+
+let mocks = {};
+const resetMocks = () => { mocks = {}; };
+const mockKey = (method, path) => `${method.toUpperCase()} ${path}`;
+const saveMock = (mockBehavior) => {
+  const {
+    method, path, status, response,
+  } = mockBehavior;
+  if (method === undefined ||
+    path === undefined ||
+    !/^\//.test(path) ||
+    status === undefined) {
+    throw new Error('invalid mock behavior supplied');
+  }
+  const mockResponse = { status };
+  if (response !== undefined) {
+    mockResponse.response = response;
+  }
+  mocks[mockKey(method, path)] = mockResponse;
+};
+const getMockResponse = (method, path) => mocks[mockKey(method, path)];
+
+const handle = (req, res) => {
+  // check for a mocking call
+  if (/\/mock$/.test(req.path)) {
+    if (req.method === 'DELETE') {
+      resetMocks();
+    } else if (req.method === 'PUT') {
+      saveMock(req.body);
+    } else {
+      res.sendStatus(501).end(); // Not Implemented
+      return;
+    }
+    res.sendStatus(204).end(); // No Content (but we're good)
+    return;
+  }
+
+  // check for an invalid path
+  if (req.swagger.path === null || req.swagger.path === undefined) {
+    res.sendStatus(501).end(); // Not Implemented
+    return;
+  }
+
+  // checked for mocked bhavior
+  const mockedResponse = getMockResponse(req.method, req.swagger.pathName);
+  if (mockedResponse) {
+    res.status(mockedResponse.status);
+    if (mockedResponse.response !== undefined) {
+      res.send(mockedResponse.response);
+    }
+    res.end();
+    return;
+  }
+
+  // check for default behavior
+  const { responses } = req.swagger.path[req.method.toLowerCase()];
+  const exampleResponseHttpCode = findKey(responses, r => getOrDefault(r, 'schema.example') !== undefined);
+  if (exampleResponseHttpCode !== undefined) {
+    res.send(responses[exampleResponseHttpCode].schema.example).end();
+    return;
+  }
+
+  res.sendStatus(501).end(); // Not Implemented
+};
 
 if (port === undefined || swaggerPath === undefined) {
   throw new Error('PORT and SWAGGER_SPEC environment variables must be set.');
@@ -21,17 +86,11 @@ fs.stat(swaggerPath, (err) => {
 const swaggerInterceptor = interceptor((req, res) => ({
   isInterceptable: () => true,
   intercept: () => {
-    if (req.swagger.path === null || req.swagger.path === undefined) {
-      res.sendStatus(501).end(); // Not Implemented
-      return;
-    }
-    const { responses } = req.swagger.path[req.method.toLowerCase()];
-    const exampleResponseHttpCode = findKey(responses, r => getOrDefault(r, 'schema.example') !== undefined);
-
-    if (exampleResponseHttpCode !== undefined) {
-      res.send(responses[exampleResponseHttpCode].schema.example).end();
-    } else {
-      res.sendStatus(501).end(); // Not Implemented
+    try {
+      handle(req, res);
+    } catch (err) {
+      console.log(err); // eslint-disable-line no-console
+      res.sendStatus(500).end(); // Oops
     }
   },
 }));
@@ -43,6 +102,8 @@ swaggerMiddleware(swaggerPath, app, (err, middleware) => {
 
   app.use(
     swaggerInterceptor,
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json(),
     middleware.metadata(),
     middleware.CORS(),
     middleware.files(),
