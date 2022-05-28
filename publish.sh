b388ecd7e477ce77c2c4ec6f95c668a8718be136
Mustafa Rizvi <mstfrzv@gmail.com>
2022-05-28 16:43:01 -0400

    chore: multiarch push script change

--- publish.sh
+++ publish.sh
@@ -50,5 +50,5 @@ git push origin $version
 docker buildx create --use
 docker buildx build \
 --push \
---platform linux/amd64,linux/arm64,linux/ppc64le \
+--platform linux/amd64,linux/arm64 \
 --tag $docker_image_name:$version .
